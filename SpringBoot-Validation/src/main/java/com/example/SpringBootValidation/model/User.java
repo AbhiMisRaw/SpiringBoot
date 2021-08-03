beaa60ac056513b75327feeffd2ed08fa83585ec
AbhiMisRaw <88389429+AbhiMisRaw@users.noreply.github.com>
2021-08-03 20:02:38 +0530

    Add files via upload

---
+++ SpringBoot-Validation/src/main/java/com/example/SpringBootValidation/model/User.java
@@ -0,0 +1,38 @@
+package com.example.SpringBootValidation.model;
+import javax.validation.constraints.NotBlank;
+
+public class User {
+	
+	@NotBlank(message ="User name cannot be empty")
+	private String name;
+	
+	@NotBlank(message ="Password cannot be empty")
+	private String password;
+	
+	private Double grade;
+	public User(String name,String password ,Double grade) {
+		this.name = name;
+		this.password = password;
+		this.grade = grade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Double getGrade() {
+		return grade;
+	}
+	public void setGrade(Double grade) {
+		this.grade = grade;
+	}
+	
+	
+}
